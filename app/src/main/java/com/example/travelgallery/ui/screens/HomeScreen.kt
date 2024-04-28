ab15c4185cbb6ed3cc68717572e4d680dc573488
tera330 <nonchan877tech@gmail.com>
2024-04-28 16:34:30 +0900

    マップ用の仮スクリーンとピン追加ボタンを仮で配置

---
+++ app/src/main/java/com/example/travelgallery/ui/screens/HomeScreen.kt
@@ -0,0 +1,52 @@
+package com.example.travelgallery.ui.screens
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+        ) {
+            Text(text = "マップ配置")
+            Column(
+                modifier = modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.End,
+            ) {
+                FloatingActionButton(
+                    modifier = modifier.padding(10.dp),
+                    shape = CircleShape,
+                    containerColor = Color.White,
+                    contentColor = Color.Black,
+                    onClick = { /*TODO*/ }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Place,
+                        contentDescription = "Place"
+                    )
+                }
+            }
+        }
+}
+
+@Preview
+@Composable
+private fun PreviewHomeScreen() {
+    HomeScreen()
+}
