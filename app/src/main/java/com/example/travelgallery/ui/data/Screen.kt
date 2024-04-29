fca922856e9815ec2ad34d866e7553cd552316f8
tera330 <nonchan877tech@gmail.com>
2024-04-29 15:15:10 +0900

    ボトムナビゲーションバーの部品を作成

---
+++ app/src/main/java/com/example/travelgallery/ui/data/Screen.kt
@@ -0,0 +1,11 @@
+package com.example.travelgallery.ui.data
+
+import androidx.annotation.StringRes
+import androidx.compose.ui.graphics.vector.ImageVector
+
+enum class Screen() {
+    Splash,
+    Home,
+    AllGalleryScreen,
+    GalleryScreen
+}
